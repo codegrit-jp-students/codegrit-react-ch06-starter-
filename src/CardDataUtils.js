c3c40ca0460424c521de8732003c66ef47e76208
kkhs0603 <pont3699@gmail.com>
2019-07-07 11:41:42 +0900

    react-ch05 update

--- src/CardDataUtils.js
+++ src/CardDataUtils.js
@@ -2,20 +2,33 @@ import imageOne from './images/image-one.jpg';
 import imageTwo from './images/image-two.jpg';
 import avatarOne from './images/avatar-one.png';
 import avatarTwo from './images/avatar-two.png';
+import * as moment from 'moment';
 
 const cardDataOne = {
   id: 1,
   poster: "posteruser1",
   posterAvatar: avatarOne,
   imageUrl: imageOne,
-  commentOne: {
-    poster: "commenter1",
-    body: "This is the insta-card"
-  },
-  commentTwo: {
-    poster: "commenter2",
-    body: "So cool!"
-  }
+  comments: [
+    {
+      postedAt: moment().format('x'), // 投稿時間
+      poster: "posterone",
+      body: "This is the insta-card"
+    },
+    {
+      postedAt: moment().format('x'), // 投稿時間
+      poster: "postertwo",
+      body: "So cool!"
+    },
+  ]
+  // commentOne: {
+  //   poster: "commenter1",
+  //   body: "This is the insta-card"
+  // },
+  // commentTwo: {
+  //   poster: "commenter2",
+  //   body: "So cool!"
+  // }
 }
 
 const cardDataTwo = {
@@ -23,14 +36,26 @@ const cardDataTwo = {
   poster: "posteruser2",
   posterAvatar: avatarTwo,
   imageUrl: imageTwo,
-  commentOne: {
-    poster: "commenter3",
-    body: "Nice photo!"
-  },
-  commentTwo: {
-    poster: "commenter4",
-    body: "Cute cat!"
-  }
+  comments: [
+    {
+      postedAt: moment().format('x'), // 投稿時間
+      poster: "commenter3",
+      body: "Nice photo!"
+    },
+    {
+      postedAt: moment().format('x'), // 投稿時間
+      poster: "commenter4",
+      body: "Cute cat!"
+    },
+  ],
+  // commentOne: {
+  //   poster: "commenter3",
+  //   body: "Nice photo!"
+  // },
+  // commentTwo: {
+  //   poster: "commenter4",
+  //   body: "Cute cat!"
+  // }
 }
 
 export const fetchData = (id = 1) => {
